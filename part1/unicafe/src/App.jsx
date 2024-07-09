b28b77ec971fb8df004cd33a6aa5e9245d632540
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 01:24:42 +0200

    rename file directory

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+
+export default App
