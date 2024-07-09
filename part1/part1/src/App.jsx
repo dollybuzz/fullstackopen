45146bdb32493c2728e4b546d42dbfe22824cd0d
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 00:44:11 +0200

    mod to show component used multiple times

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -10,10 +10,16 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-
+      <Hello />
+      <Hello />
       <Hello />
     </div>
   )
 }
-export default App
 
+
+
+
+
+
+export default App
