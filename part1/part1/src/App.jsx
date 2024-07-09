013d41c705cd59d979107efb1d70c4257333c13a
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 00:51:25 +0200

    creating props and passing data

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,22 +1,30 @@
-const Hello = () => {
+const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
-
 
 
 
