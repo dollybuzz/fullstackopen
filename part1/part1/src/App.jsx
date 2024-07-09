4bfba94187c0664c903cbab517e3d231752bf152
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 01:04:04 +0200

    add Footer component, add fragment to wrap elements without root elememt or wrapper node

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -11,18 +11,25 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
 
+const App = () => {
   const name = 'Peter'
   const age = 10
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
-    </div>
+      <Footer />
+    </>
   )
 }
 
