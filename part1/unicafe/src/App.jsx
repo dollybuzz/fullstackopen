7cc8fd658086147c477bc999807aa01f40f6ef1f
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 01:57:43 +0200

    remove assets folder, refactor to add Header component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,33 +1,81 @@
-const Hello = (props) => {
-
-  console.log(props)
+const Header = (input) => {
   return (
     <div>
-      <p>
+      <h1>{input.course}</h1>
+    </div>
+  )
+}
 
-        Hello {props.name}, you are {props.age} years old
+/*
+const Content = () => {
+  return (
+    <div>
+       <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
       </p>
     </div>
   )
 }
 
-const Footer = () => {
+const Total = () => {
   return (
     <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
-}
+}*/
+
 
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const course = 'Half Stack application development'
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <div>
-      <p>{friends}</p>
-    </div>
+    <>
+      <Header course={course} />
+    </>
   )
 }
 
+/*
+
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}
+      </p>
+    
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
+*/
 
 export default App
