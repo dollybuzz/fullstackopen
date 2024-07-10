934ac9f5d5af0dd8726679f06b3a9c720aa61d4d
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-07-10 02:19:45 +0200

    complete Content and Total component, exercise 1.1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,35 +6,27 @@ const Header = (input) => {
   )
 }
 
-/*
-const Content = () => {
+const Content = (input) => {
   return (
     <div>
        <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
+        {input.part} {input.exercise}
       </p>
     </div>
   )
 }
 
-const Total = () => {
+const Total = (input) => {
   return (
     <div>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {input.exercise1 + input.exercise2 + input.exercise3}</p>
     </div>
   )
-}*/
+}
 
 
 const App = () => {
   const course = 'Half Stack application development'
-
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -45,37 +37,12 @@ const App = () => {
   return (
     <>
       <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
     </>
   )
 }
-
-/*
-
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}
-      </p>
-    
-
-const App = () => {
-  // const-definitions
-  const course = 'Half Stack application development'
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content ... />
-      <Total ... />
-    </div>
-  )
-}
-*/
 
 export default App
