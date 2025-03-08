9d16ce018702ce944923aa2f5a51e3acfc6e68b2
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-03-08 16:35:22 +0100

    working on Part 1 C Component state, event handlers

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,46 +1,25 @@
 //Following course on fullstackopen.com
 
-//Exercise 1.5 Change the course and its parts into a single Javascript object. Fix everything that breaks.
-const Single = (input) => {
+const Hello = (props) => {
   return (
     <div>
-      <h1>{input.h1}</h1>
-      <p>{input.n0} {input.e0}</p>
-      <p>{input.n1} {input.e1}</p>
-      <p>{input.n2} {input.e2}</p>
-      <p>Number of exercises {input.total0 + input.total1 + input.total2}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-
 const App = () => {
- const course = {
-  name: 'Half Stack application development',
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
- }
+  const name = 'Peter';
+  const age = 10;
 
   return (
-    <>
-      <Single h1={course.name} 
-      n0={course.parts[0].name} e0={course.parts[0].exercises} 
-      n1={course.parts[1].name} e1={course.parts[1].exercises} 
-      n2={course.parts[2].name} e2={course.parts[2].exercises}
-      total0={course.parts[0].exercises} total1={course.parts[1].exercises} total2={course.parts[2].exercises}/>
-    </>
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   )
 }
 
