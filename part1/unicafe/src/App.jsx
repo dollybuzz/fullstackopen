5af405b08ec6fe362135f3578bb18a16a099cd9e
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-03-08 16:40:33 +0100

    adding component helper function

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,17 @@
 //Following course on fullstackopen.com
 
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow = props.age;
+  }
+
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
