bd39d0b3b86a4711aa30625113416651a15da7f5
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-03-11 17:53:26 +0100

    destructuring parameters

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,12 @@
 //Following course on fullstackopen.com
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow = props.age;
-  }
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age;
 
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
