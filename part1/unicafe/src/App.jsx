b3d65ec9be6eebfdfbf89f35fc7046204488b1ca
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-03-15 16:50:15 +0100

    define event handlers to functions

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,11 +5,15 @@ import {useState} from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  const increaseByOne = () => setCounter(counter+1);
+
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter+1)}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
     </div>
   )
 }
