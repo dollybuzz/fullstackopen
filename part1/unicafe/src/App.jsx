6690849430f0ef26b702883e7b132653ea7f8020
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-03-15 16:26:07 +0100

    use React's state hook component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,11 @@
 //Following course on fullstackopen.com
 
-const App = (props) => {
-  const {counter} = props;
+import {useState} from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter+1), 1000);
 
   return (
     <div>{counter}</div>
--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -1,18 +1,5 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
-import App from './App'
+import App from './App';
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
