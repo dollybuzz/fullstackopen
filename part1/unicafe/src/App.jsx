a4f7da335ba8d5bcfb66e28a41e6c4d0b8045ed7
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-04-19 22:44:28 +0200

    add display component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,16 +2,21 @@
 
 import {useState} from 'react';
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   const increaseByOne = () => setCounter(counter+1);
-
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
       <button onClick={increaseByOne}>plus</button>
       <button onClick={setToZero}>zero</button>
     </div>
