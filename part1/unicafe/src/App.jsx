1385043b87e3331239e55b6b0678e41725286698
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-04-20 20:54:01 +0200

    managing more complex state

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,18 +9,15 @@ import {useState} from 'react';
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increaseByOne = () => setCounter(counter+1);
-  const decreaseByOne = () => setCounter(counter-1);
-  const setToZero = () => setCounter(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text="plus"/>
-      <Button onClick={setToZero} text="zero"/>
-      <Button onClick={decreaseByOne} text="minus"/>
+     {left}
+     <button onClick={() => setLeft(left + 1)}>left</button>
+     <button onClick={() => setRight(right + 1)}>right</button>
+     {right}
     </div>
   )
 }
