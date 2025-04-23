ec325e85f9067b0154049dd20a451586505f3dcb
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-04-23 04:39:03 +0200

    manually manage state as a saved score in object with event handlers

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,15 +9,33 @@ import {useState} from 'react';
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks);
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-     {left}
-     <button onClick={() => setLeft(left + 1)}>left</button>
-     <button onClick={() => setRight(right + 1)}>right</button>
-     {right}
+     {clicks.left}
+     <button onClick={handleLeftClick}>left</button>
+     <button onClick={handleRightClick}>right</button>
+     {clicks.right}
     </div>
   )
 }
