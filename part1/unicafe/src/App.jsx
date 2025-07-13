75266a9e699bc4e62f4382291f146049256333eb
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-07-13 22:15:16 +0200

    dealing with asynchronous state

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,26 +12,21 @@ const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState([]);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
     setLeft(left + 1);
+    setTotal(updatedLeft + right);
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
     setRight(right + 1);
+    setTotal(left + updatedRight);
   }
-
-  /*using the push method then updating the state also works
-  ex:
-  const handleLeftClick = () => {
-    allClicks.push('L');
-    setAll(allClicks);
-    setLeft(left + 1);
-    }
-    however, do not do this since state of React components should never be modified directly
-  */
 
   return (
     <div>
@@ -40,6 +35,7 @@ const App = () => {
      <button onClick={handleRightClick}>right</button>
      {right}
      <p>{allClicks.join(' ')}</p>
+     <p>total {total}</p>
     </div>
   )
 }
