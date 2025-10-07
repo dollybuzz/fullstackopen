34366bcd0b2d0a961f8c3603d2fdb44d3e9c1311
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-10-07 16:30:17 -0500

    generate random anecdotes

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -49,17 +49,34 @@ const Statistics = (props) => {
 }
 
 const App = () => {
-  //save clicks of each button to its own state
+  
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
   
   const setToGood = (value) => { setGood(value) };
   const setToNeutral = (value) => { setNeutral(value) };
   const setToBad = (value) => { setBad(value) };
+  const setToSelected = (value) => setSelected(value);
 
   return (
     <div>
+      <Title title={'anecdotes'} />
+      <div>{anecdotes[selected]}</div><br/>
+      <Button onClick={() => {setToSelected(Math.trunc(Math.random()*8))}} text={"next anecdote"}/>
+      
       <Title title={'give feedback'} />
       <Button onClick={() => {setToGood(good + 1)}} text={'good'}/>
       <Button onClick={() => {setToNeutral(neutral + 1)}} text={'neutral'}/>
